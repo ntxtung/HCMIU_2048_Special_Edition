41df618e7c97183d8cc109680099aae62ba93660
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-02-04 14:16:47 +0700

    Login form

--- src/DSA2048/DSA2048Controller.java
+++ src/DSA2048/DSA2048Controller.java
@@ -1,5 +1,23 @@
 package DSA2048;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
 public class DSA2048Controller {
 	
+	 @FXML
+	    private TextField txtUsername;
+
+	    @FXML
+	    private TextField txtPassword;
+
+	    @FXML
+	    void onLogin(ActionEvent event) {
+	    	Alert al = new Alert(AlertType.INFORMATION);
+	    	al.setContentText("LOGGING IN");
+	    	al.show();
+	    }
 }
