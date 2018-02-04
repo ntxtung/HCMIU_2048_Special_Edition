cf7085e41be2375f6137fa0dbd10099982379c13
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-02-04 15:32:28 +0700

    Revert "Delete Main.java"
    This reverts commit 03723669a8492d485f286cf48ddb4dce2014f8fc.

---
+++ src/DSA2048/Main.java
@@ -0,0 +1,37 @@
+package dsa2048;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("DSA2048.fxml"));
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("UI.css").toExternalForm());
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.setScene(scene);
+			
+			primaryStage.show();
+
+			// Set window to screeen center
+			Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+			primaryStage.setX((screenBounds.getWidth() - primaryStage.getWidth())/2);
+			primaryStage.setY((screenBounds.getHeight() - primaryStage.getHeight())/2);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
