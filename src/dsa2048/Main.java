da23a9019cbe973bf5edd8a5b16e78a62ad63cd4
Nguyễn Thanh Xuân Tùng <xuantung.1998@gmail.com>
2018-05-08 09:21:38 +0700

    Commit

--- src/dsa2048/Main.java
+++ src/dsa2048/Main.java
@@ -1,36 +1,38 @@
 package dsa2048;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("fxml/Gameplay.fxml"));
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("fxml/Gameplay.fxml"));
 			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
-			
+
 			scene.getStylesheets().add(getClass().getResource("css/Gameplay.css").toExternalForm());
-//			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			// primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setTitle("2048");
 			primaryStage.setScene(scene);
-			
+
 			primaryStage.setResizable(false);
 			primaryStage.show();
 
-//			// Set window to screeen center
-//			Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-//			primaryStage.setX((screenBounds.getWidth() - primaryStage.getWidth())/2);
-//			primaryStage.setY((screenBounds.getHeight() - primaryStage.getHeight())/2);
-			
-		} catch(Exception e) {
+			// Set window to screeen center
+			Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+			primaryStage.setX((screenBounds.getWidth() - primaryStage.getWidth()) / 2);
+			primaryStage.setY((screenBounds.getHeight() - primaryStage.getHeight()) / 2);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/dsa2048/MultiplayerController.java
+++ src/dsa2048/MultiplayerController.java
@@ -1,5 +1,25 @@
 package dsa2048;
 
-public class MultiplayerController {
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class MultiplayerController extends GameplayController{
+
+    @FXML
+    private Pane paneMain;
+
+    @FXML
+    private GridPane paneGame;
+
+    @FXML
+    private Pane paneScoreCurrent;
+
+    @FXML
+    private Text scoreCurrent;
+
+    @FXML
+    private Text textGameTitle;
 
 }
